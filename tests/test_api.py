1354b62d6d1f20d4a775d5b97c18457b559a3bc8
Zad-Walid <zadwalid06@gmail.com>
2025-05-31 01:05:40 +0300

    feat(api): added testing

---
+++ tests/test_api.py
@@ -0,0 +1,58 @@
+
+from fastapi.testclient import TestClient
+from api.main import app
+import joblib
+import os
+
+client = TestClient(app)
+
+def test_model_loading():
+    assert os.path.exists("api/output/model.pkl"), "Model not found"
+    model = joblib.load("api/output/model.pkl")
+    assert model is not None, "Failed to load model"
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hand Gesture Model API is running."}
+
+def test_health_status():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
+
+def test_predict():
+    
+    example_landmarks = {
+  "landmarks": [
+    0.0, 0.0, 0.0,
+    0.1, -0.1, 0.05,
+    0.2, -0.2, 0.04,
+    0.3, -0.1, 0.03,
+    0.4, 0.0, 0.02,
+    0.5, 0.1, 0.01,
+    0.6, 0.2, 0.0,
+    0.7, 0.3, -0.01,
+    0.6, 0.4, -0.02,
+    0.5, 0.5, -0.03,
+    0.4, 0.6, -0.04,
+    0.3, 0.7, -0.05,
+    0.2, 0.6, -0.06,
+    0.1, 0.5, -0.07,
+    0.0, 0.4, -0.08,
+    -0.1, 0.3, -0.09,
+    -0.2, 0.2, -0.1,
+    -0.3, 0.1, -0.11,
+    -0.4, 0.0, -0.12,
+    -0.5, -0.1, -0.13,
+    -0.6, -0.2, -0.14
+  ]
+}
+
+    response = client.post("/predict", json=example_landmarks)    
+
+    assert response.status_code == 200
+    assert "prediction" in response.json()
+    
+    # Check if the prediction is a string (gesture name)
+    assert isinstance(response.json()["prediction"], str)
